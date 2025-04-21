542d986abeac27bfd575fcb64ad86d4c765905a8
Nick <nicholascoiner@gmail.com>
2025-04-21 15:31:19 -0700

    removed target folders and files and ignored

--- guessing_game/target/debug/deps/cfg_if-2b1095f2195c1f61.d
+++
@@ -1,7 +0,0 @@
-/home/nick/projects/guessing_game/target/debug/deps/libcfg_if-2b1095f2195c1f61.rmeta: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/nick/projects/guessing_game/target/debug/deps/libcfg_if-2b1095f2195c1f61.rlib: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/nick/projects/guessing_game/target/debug/deps/cfg_if-2b1095f2195c1f61.d: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs:
--- guessing_game/target/debug/deps/rand_chacha-9feb1e95a73ac15c.d
+++
@@ -1,7 +0,0 @@
-/home/nick/projects/guessing_game/target/debug/deps/librand_chacha-9feb1e95a73ac15c.rmeta: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/home/nick/projects/guessing_game/target/debug/deps/rand_chacha-9feb1e95a73ac15c.d: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
-/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
-/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
