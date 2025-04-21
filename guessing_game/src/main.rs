b4823d68fb9f5cbc6f2aa63666c5ae6184059a3f
Nick <nicholascoiner@gmail.com>
2025-04-21 15:27:17 -0700

    Added random guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
 
@@ -6,11 +8,22 @@ fn main() {
 
     println!("Please input your guess.");
     
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
     let mut guess = String::new();
-    
-    io::stdin()
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    
+    let guess : u32 = guess
+        .trim()
+        .parse()
+        .expect("Please enter a number");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("That's correct!"),
+    };
+
     println!("You guessed: {guess}");
 }
---
+++ guessing_game/target/debug/deps/rand_chacha-cf1cd6376f09ec46.d
@@ -0,0 +1,9 @@
+/home/nick/projects/guessing_game/target/debug/deps/librand_chacha-cf1cd6376f09ec46.rmeta: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/nick/projects/guessing_game/target/debug/deps/librand_chacha-cf1cd6376f09ec46.rlib: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/nick/projects/guessing_game/target/debug/deps/rand_chacha-cf1cd6376f09ec46.d: /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
+/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
+/home/nick/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
