0e3c8ef6d8bacff7bd79833bd4354ef2174b9d13
Nick <nicholascoiner@gmail.com>
2025-04-21 21:41:42 -0700

    Removed files that should have been ignored

--- guessing_game/target/debug/deps/guessing_game-807f5fd7609dec2b.d
+++
@@ -1,5 +0,0 @@
-/home/nick/projects/guessing_game/target/debug/deps/guessing_game-807f5fd7609dec2b: src/main.rs
-
-/home/nick/projects/guessing_game/target/debug/deps/guessing_game-807f5fd7609dec2b.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/nick/projects/guessing_game/target/debug/guessing_game: /home/nick/projects/guessing_game/src/main.rs
