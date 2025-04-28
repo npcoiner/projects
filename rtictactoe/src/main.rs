45ce25c6d0d3aa694fc60b22f2a7f8178dd978f3
Nick <nicholascoiner@gmail.com>
2025-04-28 12:04:40 -0700

    clear screen functionality working

--- rtictactoe/src/main.rs
+++ rtictactoe/src/main.rs
@@ -4,13 +4,19 @@ use crossterm::terminal::disable_raw_mode;
 use std::io::Write;
 
 fn main() {
-    enable_raw_mode().unwrap();
+    enable_raw_mode().expect("Failed to enter raw_mode");
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        print!("{}\r\n", c);
-        io::stdout().flush().unwrap();
+        let c = b.unwrap();
+        print!("\x1B[2J\x1B[3J\x1B[H");
+        print!("Binary: {0:08b} ASCII: {0:#03}\r\n", c);
+        print!("\x1B[5A"); // Move cursor up 5 lines
 
-        if c == 'q' {
+        print!("\x1B[3C"); // Move cursor right 3 columns
+        
+
+        io::stdout().flush().expect("Failed to flush");
+
+        if c as char == 'q' {
 			disable_raw_mode().unwrap();
             break;
         }
