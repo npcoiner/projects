29524a1682e67462c581cec350d8cb10978dd469
Nick <nicholascoiner@gmail.com>
2025-04-28 10:56:10 -0700

    Raw mode implemented

--- rtictactoe/src/main.rs
+++ rtictactoe/src/main.rs
@@ -1,8 +1,19 @@
+use std::io::{self, Read};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+use std::io::Write;
+
 fn main() {
-    //Simple tic tac toe game
-    loop{
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        print!("{}\r\n", c);
+        io::stdout().flush().unwrap();
+
+        if c == 'q' {
+			disable_raw_mode().unwrap();
             break;
-
-
+        }
     }
 }
+
