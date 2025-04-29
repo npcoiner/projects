4d659b60a91e6d5d97aa0a7234fc6df1310d065b
Nick <nicholascoiner@gmail.com>
2025-04-29 16:38:12 -0700

    Formatted and added functions

--- rtictactoe/src/main.rs
+++ rtictactoe/src/main.rs
@@ -3,15 +3,35 @@ use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
 use std::io::Write;
 
+static WIDTH: i8 = 32;
+static HEIGHT: i8 = 32;
+
+fn init(){
+     enable_raw_mode().expect("Failed to enter raw_mode");
+    print!("\x1B[2J\x1B[3J\x1B[H");//Clear the screen, clear the scroll buffer, reset cursor
+                                   //position.
+    print!("q to quit\r\n");
+    io::stdout().flush().expect("Failed to flush");
+}
+
+fn clearscrn(){
+     print!("\x1B[2J\x1B[3J\x1B[H");//Clear the screen, clear the scroll buffer, reset cursor
+                                       //position.
+}
+
+
+
 fn main() {
-    enable_raw_mode().expect("Failed to enter raw_mode");
+    init();
+
     for b in io::stdin().bytes() {
         let c = b.unwrap();
-        print!("\x1B[2J\x1B[3J\x1B[H");
+        clearscrn();
+        print!("q to quit\r\n");
         print!("Binary: {0:08b} ASCII: {0:#03}\r\n", c);
-        print!("\x1B[5A"); // Move cursor up 5 lines
+        //print!("\x1B[5A"); // Move cursor up 5 lines
 
-        print!("\x1B[3C"); // Move cursor right 3 columns
+        //print!("\x1B[3C"); // Move cursor right 3 columns
         
 
         io::stdout().flush().expect("Failed to flush");
